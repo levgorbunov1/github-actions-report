2ab7c35c13df654e31781005085b18eb8c143c20
Lev Gorbunov <lev.gorbunov@digital.cabinet-office.gov.uk>
2025-07-07 14:00:29 +0100

    get all workflow file paths

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
 import os
+import subprocess
+from pathlib import Path
+import glob
 
 def clone_repo(repo_url: str, clone_dir: str = "cloned_repos") -> str:   
     cloned_repo_dir = f"{clone_dir}/{repo_url.rstrip("/").split("/")[-1]}"
@@ -13,6 +15,22 @@ def clone_repo(repo_url: str, clone_dir: str = "cloned_repos") -> str:
 
     return cloned_repo_dir
 
+def find_workflow_files(repo_path: str) -> list[Path]:
+    workflows_path = repo_path + "/.github/workflows"
+
+    if not Path(workflows_path).exists():
+        print(f"Can't find {repo_path}.github/workflows directory")
+        return []
+
+    print(f"searching {workflows_path} for workflows")
+
+    return list(glob.glob(workflows_path + "/*.yml")) + list(glob.glob(workflows_path + "/*.yaml"))
+
+def analyze_github_actions(repo_url: str) -> tuple[list[str], list[str]]:
+    repo_path = clone_repo(repo_url)
+    workflow_files = find_workflow_files(repo_path)
+
+
 if __name__ == "__main__":
     repo_url = "https://github.com/levgorbunov1/my-eks-cluster"
-    repo_path = clone_repo(repo_url)
+    analyze_github_actions(repo_url)
